07fb71b8502ffa7b3b08eb3e5a02f5cc8e638a90
Maksymilian <maksymilian.official@gmail.com>
2025-02-16 20:03:36 +0100

    Dodanie poruszania się w prawo i lewo za pomocą strzałek

--- ship.py
+++ ship.py
@@ -14,9 +14,23 @@ class Ship:
         #Każdy nowy statek kosmiczne pojawia się na dole ekranu.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Opcje wskazujące na poruszanie się statku
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Uaktualnienie położenia statku na podstawie opcji wskazującej na jego ruch"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
+
     def blitme(self):
         """Wyświetlenie statku kosmicznego w jego aktualnym położeniu"""
         self.screen.blit(self.image, self.rect)
+
+
     
 # class AlienShip:
 #     """Klasa statku obcych"""
